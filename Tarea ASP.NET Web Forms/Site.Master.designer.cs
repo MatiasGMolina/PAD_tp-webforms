80a4ba86c17f2b1d8680529a91a41a6beafbc5cf
Matias Molina <Matias.Molina@alu.frt.utn.edu.ar>
2026-09-14 12:56:37 -0300

    Punto 2: titulo del sitio web y marca con logo en la master page

--- Tarea ASP.NET Web Forms/Site.Master.designer.cs
+++ Tarea ASP.NET Web Forms/Site.Master.designer.cs
@@ -15,15 +15,6 @@ namespace Tarea_ASP.NET_Web_Forms
     {
 
         /// <summary>
-        /// HeadContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
